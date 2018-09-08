4129c070de66076d674115f9f04b3400a64f0ab4
Jeff Oliver <jeffoliver3384@gmail.com>
2018-09-07 21:06:58 -0400

    Mutate an Array Declared with const

---
+++ Mutate an Array Declared with const/main.js
@@ -0,0 +1,13 @@
+const s = [5, 7, 2];
+function editInPlace() {
+  'use strict'
+  // Change code below this line
+
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+
+  // Change code below this line
+}
+
+editInPlace();
