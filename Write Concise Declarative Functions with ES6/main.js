103f87d74676cde75c3225714ce1b94a0eeb47be
Jeff Oliver <jeffoliver3384@gmail.com>
2018-10-07 18:28:39 -0400

    Write Concise Declarative Functions with ES6

---
+++ Write Concise Declarative Functions with ES6/main.js
@@ -0,0 +1,11 @@
+// Change code below this line
+const bicycle = {
+  gear: 2,
+  setGear (newGear) {
+    'use strict';
+    this.gear = newGear;
+  }
+};
+// Change code above this line
+bicycle.setGear(4);
+console.log(bicycle.gear);
